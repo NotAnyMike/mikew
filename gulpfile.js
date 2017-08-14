72b19088bd5ca8a75b6e0d669a1497adf6848d5c
Mike W <mwc1.0@hotmail.com>
2017-08-14 17:36:39 -0500

    updating footer, mainCOntainer, working on 'some works'

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,15 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var refresh = require('gulp-refresh');
+var nib = require('nib');
 
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('stylus-dev', function(){
-	return gulp.src('./web/layout/stylus/main.styl') .pipe(stylus())
-	.pipe(gulp.dest('./web/layout/css'))
-	.pipe(refresh());
-});
-
-gulp.task('sourcemaps-inline', function () {
-  return gulp.src('./web/layout/stylus/main.styl')
-    .pipe(sourcemaps.init())
-    .pipe(stylus())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./web/layout/css/main'));
+	return gulp.src('./web/layout/stylus/main.styl')
+		.pipe(stylus({'include css':true, use: nib()}))
+		.pipe(gulp.dest('./web/layout/css'))
+		.pipe(refresh());
 });
 
 gulp.task('w-stylus', function(){
